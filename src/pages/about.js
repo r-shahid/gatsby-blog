ec8a1b2c13ebd552451e79b609724ac066439ae9
Riana <rianashahid@gmail.com>
2020-12-20 01:00:28 -0500

    about page done

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { graphql, StaticQuery } from "gatsby"
-import Img from "gatsby-image"
+import Img from "./../../content/assets/riana-in-bk.jpg"
+import resume from "./../../content/assets/resume-snippet.png"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -12,47 +13,54 @@ const AboutPage = ({ data }, location) => {
   const siteTitle = data.site.siteMetadata.title
 
   return (
-    <Layout title={siteTitle}>
-      <SEO title="About" keywords={[`blog`, `gatsby`, `javascript`, `react`]} />
+		<Layout title={siteTitle}>
+			<SEO title='About' keywords={[`blog`, `gatsby`, `javascript`, `react`]} />
 
-      <article className="post-content page-template no-image">
-        <div className="post-content-body">
-          <h2 id="clean-minimal-and-deeply-customisable-london-is-a-theme-made-for-people-who-appreciate-simple-lines-">
-            Clean, minimal, and deeply customisable. London is a theme made for
-            people who appreciate simple lines.
-          </h2>
-          <figure className="kg-card kg-image-card kg-width-full">
-            <Img
-              fluid={data.benchAccounting.childImageSharp.fluid}
-              className="kg-image"
-            />
-            <figcaption>Large imagery is at the heart of this theme</figcaption>
-          </figure>
-          <h3 id="dynamic-styles">Dynamic styles</h3>
-          <p>
-            London comes with photo-centric main layout best suited to
-            photography, graphics portfolios and other image-heavy uses.
-          </p>
-          <p>
-            Both post and page templates are light and minimal, with all the
-            focus on the content while the design of the theme gets out of the
-            way. Beneath the hood, London enjoys the full power of the{" "}
-            <a href="https://docs.ghost.org/api/handlebars-themes/">
-              Ghost Handlebars Theme API
-            </a>{" "}
-            to provide limitless customisation options and dynamic styles.
-          </p>
-          <p>
-            Don't forget to check out the{" "}
-            <a href="https://docs.ghost.org/integrations/">
-              Ghost Integrations Directory
-            </a>{" "}
-            for more ways to integrate Ghost with your favourite services.
-          </p>
-        </div>
-      </article>
-    </Layout>
-  )
+			<article className='post-content page-template no-image'>
+				<div className='post-content-body'>
+					<h2 id='about-header'>
+						Lifestyle blogger and full stack developer based in New York
+						City. Stay here for the latest in NYC's art world, literature, and
+						beauty & lifestyle content. Check out my developer portfolio{' '}
+						<a href='https://r-shahid.github.io' target='_blank'>
+							 here
+						</a>
+						.
+					</h2>
+					<figure className='kg-card kg-image-card kg-width-full'>
+						{/* <Img
+							// fluid={data.benchAccounting.childImageSharp.fluid}
+							// className='kg-image'
+						/> */}
+						<div className='img-container'>
+							<div>
+								<img className='about-img' src={Img} alt='riana in brooklyn' />
+								<figcaption>
+									Throwback to exploring the city in a pre-COVID time
+								</figcaption>
+							</div>
+						</div>
+					</figure>
+					<h3 id='dynamic-styles'>Hi, I'm Riana</h3>
+					<p>
+						Ever since I was young, I've had an interest in the arts. As I got older, I embraced my analytical side throughout my academic career. I have a Bachelor of Arts in Mathematics from New York University and studied software engineering at General Assembly. 
+					</p>
+					<p>
+						These days, I combine the two fields with clean code and even cleaner design. Outside of coding, my interests lie in content creation through product photography and long-form blog posts as well as painting and the fiber arts.
+					</p>
+          {/* <div className='about-bottom'>
+            <a href='https://r-shahid.github.io'>
+              <img src={resume} alt="resume and link to portfolio" />
+            </a>
+            <br />
+            Click the image to visit my portfolio page.
+            
+          </div> */}
+          
+				</div>
+			</article>
+		</Layout>
+	);
 }
 
 const indexQuery = graphql`
